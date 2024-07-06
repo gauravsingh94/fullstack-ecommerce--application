a27e5054b423828baf3adfa948135377fc9bbaa1
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-07-06 23:18:47 +0530

    added the signin page.

--- client/src/pages/SigninPage.tsx
+++ client/src/pages/SigninPage.tsx
@@ -17,21 +17,21 @@ const SigninPage = () => {
         </div>
       </div>
       {/* Sign-in section */}
-      <div className="w-[50%] h-full flex flex-col justify-center items-center">
-        <div className="flex flex-col">
-          <h1>Sign In</h1>
+      <div className="w-[50%] h-full flex flex-col justify-center items-center ">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-[40px]">Sign In</h1>
           <h1>Don’t have an account yet? Sign Up</h1>
           <input
             type="email"
             placeholder="Your email address"
-            className="mt-2 p-2 border border-gray-300 rounded"
+            className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
           />
           <input
             type="password"
             placeholder="Password"
-            className="mt-2 p-2 border border-gray-300 rounded"
+            className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
           />
-          <button className="mt-2 p-2 bg-blue-500 text-white rounded">
+          <button className="mt-2 p-2 bg-black text-white rounded">
             Sign In
           </button>
         </div>
