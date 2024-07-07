709c67c85581d9af08355b072122b2832c5eae0b
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-07-07 12:41:22 +0530

    connected signin and signup page to server.

--- client/src/pages/SigninPage/SigninForm.tsx
+++ client/src/pages/SigninPage/SigninForm.tsx
@@ -1,4 +1,43 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const SigninForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("user");
+  const navigate = useNavigate();
+
+  const handleRoleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRole(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const data = {
+      email,
+      password,
+    };
+
+    const url =
+      role === "admin"
+        ? "http://localhost:3000/admin/login"
+        : "http://localhost:3000/user/login";
+
+    try {
+      const response = await axios.post(url, data);
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        role === "admin"
+          ? navigate("/admin/product")
+          : navigate("/user/product");
+      } else {
+        alert("Incorrect username or passoword.");
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  };
+
   return (
     <div className="w-[50%] h-full flex flex-col justify-center items-center ">
       <div className="flex flex-col gap-6">
@@ -8,14 +47,39 @@ const SigninForm = () => {
           type="email"
           placeholder="Your email address"
           className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
+          onChange={(e) => setPassword(e.target.value)}
         />
+        <div className="flex gap-4">
+          <label>
+            <input
+              type="radio"
+              value="user"
+              checked={role === "user"}
+              onChange={handleRoleChange}
+            />
+            User
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="admin"
+              checked={role === "admin"}
+              onChange={handleRoleChange}
+            />
+            Admin
+          </label>
+        </div>
 
-        <button className="mt-2 p-2 bg-black text-white rounded hover:bg-white hover:text-black border border-black transition duration-300 ease-in-out">
+        <button
+          onClick={handleSubmit}
+          className="mt-2 p-2 bg-black text-white rounded hover:bg-white hover:text-black border border-black transition duration-300 ease-in-out"
+        >
           Signin
         </button>
       </div>
--- client/src/pages/SignupPage/SignupForm.tsx
+++ client/src/pages/SignupPage/SignupForm.tsx
@@ -1,5 +1,41 @@
-import RadioButton from "./RadioButton";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const SignupForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("user");
+  const navigate = useNavigate();
+  const handleRoleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRole(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    const data = {
+      name,
+      email,
+      image,
+      password,
+    };
+    console.log(data);
+
+    const url =
+      role === "admin"
+        ? "http://localhost:3000/admin/signup"
+        : "http://localhost:3000/user/signup";
+
+    try {
+      const response = await axios.post(url, data);
+      console.log(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      role === "admin" ? navigate("/admin/product") : navigate("/user/product");
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  };
+
   return (
     <div className="w-[50%] h-full flex flex-col justify-center items-center ">
       <div className="flex flex-col gap-6">
@@ -9,25 +45,50 @@ const SignupForm = () => {
           type="text"
           placeholder="Your Name"
           className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
+          onChange={(e) => setName(e.target.value)}
         />
         <input
           type="email"
           placeholder="Your email address"
           className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="text"
           placeholder="Paste Profile Image link"
           className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
+          onChange={(e) => setImage(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           className="mt-2 p-2 border-b border-gray-500 placeholder-slate-500"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <RadioButton />
-
-        <button className="mt-2 p-2 bg-black text-white rounded hover:bg-white hover:text-black border border-black transition duration-300 ease-in-out">
+        <div className="flex gap-4">
+          <label>
+            <input
+              type="radio"
+              value="user"
+              checked={role === "user"}
+              onChange={handleRoleChange}
+            />
+            User
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="admin"
+              checked={role === "admin"}
+              onChange={handleRoleChange}
+            />
+            Admin
+          </label>
+        </div>
+        <button
+          onClick={handleSubmit}
+          className="mt-2 p-2 bg-black text-white rounded hover:bg-white hover:text-black border border-black transition duration-300 ease-in-out"
+        >
           Signup
         </button>
       </div>
